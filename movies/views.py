5818ff09f888b2388d16ac048da13236c25c5189
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2023-08-10 23:46:02 -0400

    updated  with a template

--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'movies/index.html', {})
+    context = {
+        'movies': ['gladiator', 'top gun', 'cinderella']
+    }
+    return render(request, 'movies/index.html', context)
 
 def about(request):
     return render(request, 'movies/about.html', {})
